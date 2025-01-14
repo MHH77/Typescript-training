9ea4e00c683f229317ae6cac4d58c4b7a29d2f65
MHH77 <mesfandiari77@gmail.com>
2025-01-14 20:42:08 +0330

    [feature] Variables

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,8 @@
+var age = 30;
+var firstName = "Alice";
+var familename = "Mohammadi"; // اطمینان حاصل کنید که این نام درست نوشته شده باشد
+var isActive = true;
+var nullableValue = null;
+var undefinedValue = undefined;
+// استفاده صحیح از نام متغیر
+console.log("Hi ".concat(firstName, " ").concat(familename));
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,10 @@
+let age: number = 30;
+let firstName: string = "Alice";
+let familename: string = "Mohammadi"; // اطمینان حاصل کنید که این نام درست نوشته شده باشد
+let isActive: boolean = true;
+
+let nullableValue: null = null;
+let undefinedValue: undefined = undefined;
+
+// استفاده صحیح از نام متغیر
+console.log(`Hi ${firstName} ${familename}`);
