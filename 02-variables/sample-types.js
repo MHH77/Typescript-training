cde1d951683b25ee6ae9f741e5d52577a8722f8c
MHH77 <mesfandiari77@gmail.com>
2025-01-15 19:11:59 +0330

    [bug] fix bug

--- 02-variables/sample-types.js
+++ 02-variables/sample-types.js
@@ -1,8 +1,7 @@
 var age = 30;
 var firstName = "Alice";
-var familename = "Mohammadi"; // اطمینان حاصل کنید که این نام درست نوشته شده باشد
+var familename = "Mohammadi";
 var isActive = true;
 var nullableValue = null;
 var undefinedValue = undefined;
-// استفاده صحیح از نام متغیر
 console.log("Hi ".concat(firstName, " ").concat(familename));
--- 02-variables/sample-types.ts
+++ 02-variables/sample-types.ts
@@ -1,10 +1,8 @@
 let age: number = 30;
 let firstName: string = "Alice";
-let familename: string = "Mohammadi"; // اطمینان حاصل کنید که این نام درست نوشته شده باشد
+let familename: string = "Mohammadi";
 let isActive: boolean = true;
 
 let nullableValue: null = null;
 let undefinedValue: undefined = undefined;
-
-// استفاده صحیح از نام متغیر
 console.log(`Hi ${firstName} ${familename}`);
